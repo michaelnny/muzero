c19c0785ae8569ff0f18beac0655c18ef0fc2be1
Michael Hu <michael@Michaels-Mac-mini.local>
2022-06-11 09:16:23 +0800

    refactor

--- run_smoke_tests.sh
+++ run_smoke_tests.sh
@@ -4,5 +4,5 @@ set -u -e  # Check for uninitialized variables and exit if any command fails.
 
 # Run smoke tests
 python3 -m muzero.tictactoe.run_training
+python3 -m muzero.classic.run_training --environment_name=CartPole-v1
 python3 -m muzero.classic.run_training --environment_name=LunarLander-v2
-python3 -m muzero.atari.run_training_ram
